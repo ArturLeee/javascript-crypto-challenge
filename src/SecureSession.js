9a610e1ec9b0364bcae472d62f3ac32683eda49f
ArturLeee <artur.li@student.ehb.be>
2019-10-20 20:24:50 +0200

    Aanpassingen syntax en afwerken decrypt en encrypt functies

--- src/SecureSession.js
+++ src/SecureSession.js
@@ -2,6 +2,7 @@ const libsodium = require('libsodium-wrappers');
 let serverPublicKey, clientPublicKey, serverPrivateKey;
 let rx, tx;
 
+
 async function init()
 {
     await libsodium.ready;
@@ -12,20 +13,42 @@ async function init()
     serverPublicKey = keypair.publicKey;
 }
 
-async function serverPublicKey()
+
+async function ServerPublicKey()
 {
     await init();
+
     return serverPublicKey;
 }
-module.exports.serverPublicKey = serverPublicKey;
+module.exports.serverPublicKey = ServerPublicKey;
 
-function setClientPublicKey(publickey)
+
+function SetClientPublicKey(publicKey)
 {
-    if(publickey != null && (typeof(clientPublicKey) == 'undefined') || clientPublicKey == null || clientPublicKey == publickey)
+    if(publicKey != null && (typeof(clientPublicKey) == 'undefined') || clientPublicKey == null || clientPublicKey == publicKey)
     {
-        clientPublicKey = publickey;
+        clientPublicKey = publicKey;
     } else
     throw "client public key already set";
 }
-module.exports.setClientPublicKey = setClientPublicKey;
+module.exports.setClientPublicKey = SetClientPublicKey;
 
+
+function decrypt(ciphertext, nonce)
+{
+    const sharedKeys = libsodium.crypto_kx_server_session_keys(serverPublicKey, serverPrivateKey, clientPublicKey);
+
+    return libsodium.crypto_secretbox_open_easy(ciphertext, nonce, sharedKeys.sharedRx);
+}
+module.exports.decrypt = decrypt;
+
+
+function encrypt(msg)
+{
+    const sharedKeys = libsodium.crypto_kx_server_session_keys(serverPublicKey, serverPrivateKey, clientPublicKey);
+    var nonce = libsodium.randombytes_buf(libsodium.crypto_secretbox_NONCEBYTES);
+    var ciphertext = libsodium.crypto_secretbox_easy(msg, nonce, sharedKeys.sharedTx);
+
+    return{ciphertext, nonce};
+}
+module.exports.encrypt = encrypt;
